18c52aa6e387033498a8d3d2e334dad12ff56203
celinechole <github@celinechole.com>
2019-10-26 12:19:22 -0400

    create attack

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Resources
 - https://textart.io/art/tag/pokemon/1
 https://gist.github.com/MatheusFaria/4cbb8b6dbe33fd5605cf8b8f7130ba6d
+
+- https://www.pokemon.com/us/pokedex/
--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 class Pokemon {
-    constructor(name, health, attacks, power) {
-        this.name = name,
-        this.health = health,
-        this.attacks = attacks,
-        this.power = power
+    constructor(name, health, type, attacks) {
+        this.name = name;
+        this.health = health;
+        this.type = type;
+        this.attacks = attacks;
     }
 }
+
+class Attack {
+    constructor(name, power, type) {
+        this.name = name;
+        this.power = power;
+        this.type = type;
+    }
+}
+
+// class Play() {
+//     constructor() {
+//     }
+//     pokemonTurn() {
+
+//     }
+//     winner() {
+
+//     }
+// }
+
+console.log(new Pokemon('Pikachu', 40, 'Electric', new Attack('Catastropika', 'Foudre', 20)))
+console.log(new Pokemon('Bulbasaur', 35, 'Grass', new Attack('Griffe', 10)))
+console.log(new Pokemon('Charmander', 60, 'Fire', new Attack('Griffe', 25)))
+
+module.exports = { Pokemon, Attack }
+
